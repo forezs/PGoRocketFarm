d90236be877ceb423d1544da20356373a9f43345
forezs <75326550+forezs@users.noreply.github.com>
2021-08-02 10:17:38 +0300

    Add files via upload

---
+++ rocket.py
@@ -0,0 +1,20 @@
+from detector import *
+
+
+action = MainAction()
+detector = MainDetector()
+
+def main():
+    print('[INFO] Getting invasion..')
+    action.get_invasion()
+    while True:
+        detector.battle(),
+        detector.detect_poke(),
+        detector.close_pokestop(),
+        detector.open_pokestop(),
+        detector.timers_check(),
+        detector.check_exit()
+
+
+if __name__ == '__main__':
+    main()
